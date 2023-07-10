b91e0618b5157f48c7397d66ada648d3846e44c7
ErfanTaqavi <erfan.taghavi.6280@gmail.com>
2023-07-10 23:47:38 +0330

    write func to transfer money and another to close your account

--- script.js
+++ script.js
@@ -83,9 +83,10 @@ const DisplayMovments = function (movements) {
 };
 
 //function to calc Balance
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov);
-  labelBalance.textContent = `${balance} ₤`;
+const calcDisplayBalance = function (acc) {
+  acc.Balance = acc.movements.reduce((acc, mov) => acc + mov);
+
+  labelBalance.textContent = `${acc.Balance} ₤`;
 };
 
 //function to calc summry in
@@ -100,8 +101,8 @@ const calcDisplaySummary = function (account) {
   labelSumOut.textContent = `${Math.abs(out)}₤`;
   const interest = account.movements
     .filter(mov => mov > 0)
-    .map(deposit => (deposit *account.interestRate) / 100)
-    .filter(interest=> interest > 1)
+    .map(deposit => (deposit * account.interestRate) / 100)
+    .filter(interest => interest > 1)
     .reduce((acc, interest) => acc + interest);
   labelSumInterest.textContent = `${interest}₤`;
 };
@@ -119,85 +120,81 @@ const createusernames = function (accs) {
 };
 createusernames(accounts);
 
+const UpdateUI = function (acc) {
+  //Display movments
+  DisplayMovments(acc.movements);
+  //Display Balance
+  calcDisplayBalance(acc);
+  //Display summary
+  calcDisplaySummary(acc);
+};
 
-let currentAccount
+let currentAccount;
 //login form
-btnLogin.addEventListener('click',function(e){
+btnLogin.addEventListener('click', function (e) {
   //prevent form from submitting
   e.preventDefault();
 
-currentAccount = accounts.find(acc => acc.username === inputLoginUsername.value)
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
 
-if(currentAccount?.pin === Number(inputLoginPin.value)){
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    //welcome massage
+    labelWelcome.textContent = `Welcome back ${
+      currentAccount.owner.split(' ')[0]
+    }`;
 
-  //welcome massage
-  labelWelcome.textContent =`Welcome back ${currentAccount.owner.split(' ')[0]}`
-  
-  containerApp.style.opacity = 100
+    containerApp.style.opacity = 100;
 
-  //clear input fields 
-  inputLoginUsername.value = inputLoginPin.value = ''
-  inputLoginPin.blur()
+    //clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
 
-  //Display movments
-  DisplayMovments(currentAccount.movements)
-  //Display Balance
-  calcDisplayBalance(currentAccount.movements)
-  //Display summary
-  calcDisplaySummary(currentAccount)
+    UpdateUI(currentAccount);
+  }
+});
 
-}
-})
-// const deposits = movements.filter(mov => mov > 0)
-// console.log(deposits);
+//func to transfer money
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.Balance >= amount &&
+    receiverAcc?.username !== currentAccount.username
+  ) {
+    //Doing amount
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
 
-// const withdrawals = movements.filter(mov => mov < 0)
-// console.log(withdrawals);
+    UpdateUI(currentAccount);
+  }
+  //clear input fields
+  inputTransferAmount.value = inputTransferTo.value = '';
+  inputTransferAmount.blur();
+});
 
-//accumulator --> SNOWBALL
-// const balance = movements.reduce((acc,cur) => acc + cur,
-// // acc in default is zero and if you want to change it ,should write your number this place
-// )
-// console.log(balance);
+//func to close account 
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
 
-//-------------------------------------------------------------------
-//*****Coding Challenge ******/
-/* 
-Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+  if (
+    currentAccount.username === inputCloseUsername.value &&
+    currentAccount.pin === Number(inputClosePin.value)
+  ) {
+    const index = accounts.findIndex(
+      account => account.username === inputCloseUsername.value
+    );
+    accounts.splice(index,1)
+    containerApp.style.opacity = 0;
 
-Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
 
-1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old,
- humanAge = 16 + dogAge * 4.
-2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
-3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
-4. Run the function for both test datasets
-
-TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
-TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
-
-GOOD LUCK 😀
-*/
-// const calcAverageHumanAge = function(ages){
-//   const humanage = ages.map(age => (age <= 2 ?  2* age : 16 + age *4  ))
-//   const adults = humanage.filter(age => age >= 18 )
-//   const average = adults.reduce((acc,age,i,arr)=> acc+age)/adults.length
-//   console.log(humanage);
-//   console.log(adults);
-//   console.log(average);
-
-// }
-
-// calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])
-// calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])
-
-// //edit to arrow func
-// const calcAverageHumanAgeNew = function(ages){
-//   const average = ages.map(age =>(age <= 2 ?  2* age : 16 + age *4  )).filter(age => age >=18).reduce((acc,age,i,arr) => acc + age/arr.length,0)
-//   console.log(average);
-
-// }
-// calcAverageHumanAgeNew([5, 2, 4, 1, 15, 8, 3])
-// calcAverageHumanAgeNew([16, 6, 10, 5, 6, 1, 4])
-/*END CODING*/
+});
 
